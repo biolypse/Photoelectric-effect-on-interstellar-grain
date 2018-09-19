662f4039d058cfb761249ef8bf8748e7bb3999ca
Nico Bellemont <nico.bellemont@gmail.com>
2018-09-20 01:16:48 +0200

    adding some constants

--- Version Alpha/constants.py
+++ Version Alpha/constants.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 E0 = 8
-GRAIN_MAX_RADIUS = 1e-8
+GRAIN_MAX_RADIUS = 1e-5
 GRAIN_MIN_RADIUS = 3.1e-10
 LA = 1e-8
 LE = 1e-9
+Z_ELECTRON = -1
+RHO_C = 1.9
+M_C = 12.0107*1.66e-27
