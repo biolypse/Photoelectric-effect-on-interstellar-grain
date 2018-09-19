56f7dba648a0145340818b9cf859a4143c9ab4e0
Nico Bellemont <nico.bellemont@gmail.com>
2018-09-20 01:18:02 +0200

    adding the x_contact and the x_absorption fonction

--- Version Alpha/fonctions.py
+++ Version Alpha/fonctions.py
@@ -1,51 +1,78 @@
 ################################################################################
 ################################### IMPORT #####################################
 ################################################################################
+import constants as c
+from matplotlib import pyplot as plt
 import numpy as np
 from random import uniform
-from matplotlib import pyplot as plt
-
-
-################################################################################
-################################# VARIABLES ####################################
-################################################################################
-La = 100e-10
-
-
-
 ################################################################################
 ################################# FUNCTIONS ####################################
 ################################################################################
 def load(name):
     return np.loadtxt(name)
 
-def non_uniform_generator_P():
-    x = []
-    for i in range(100000):
-        a = (np.random.exponential(La))
-        x.append(a)
-        i += 1
-    plt.hist(x, bins = 1000)
-    plt.show()
+
+def non_uniform_generator_exp():
+    #x = []
+    #for i in range(100000):
+    a = (np.random.exponential(c.LA))
+        #x.append(a)
+        #i += 1
+    # plt.hist(x, bins = 1000)
+    # plt.show()
+    return a
 
 
-def non_uniform_generator_Y():
-    x = []
-    fx = []
+def non_uniform_generator_th():
+    # x = []
+    # fx = []
     for i in range(1000):
         a = uniform(3, 16)
         u = uniform(0, 1)
-        while u > 0.5 * (1 + np.tanh((a - 8) / 2)):
+        while u > 0.5 * (1 + np.tanh((a - c.E0) / 2)):
             a = uniform(3, 16)
             u = uniform(0, 1)
-        b = 0.5 * (1 + np.tanh((a - 8) / 2))
-        x.append(a)
-        fx.append(b)
+        return a
+        # b = 0.5 * (1 + np.tanh((a - 8) / 2))
+        # x.append(a)
+        # fx.append(b)
 
-    plt.plot(x, fx, '.')
-    plt.show()
+    # plt.plot(x, fx, '.')
+    # plt.show()
 
-def x_enter(row):
-    for i in range(len(row)):
-        if row[i] == 1:
-            return i
+
+def x_contact(row):
+    if sum(row) == 0:
+        print("Le photon n'a pas rencontré le grain")
+        exit = True
+        return None, exit
+    else:
+        exit = False
+        i = 0
+        while row[i] != 1:
+            i += 1
+        # for i in range(len(row)):
+        #     if row[i] == 1:
+        return i, exit
+
+
+def x_absorption(da, nb_rows, grain_size, contact_position):
+    pixel_size = grain_size / len(nb_rows)
+    print("taille d'un pixel", pixel_size)
+    if c.LA < grain_size:
+        absorption = np.round_(da / pixel_size)
+        absorption_pixel = int(contact_position + absorption)
+        return(absorption_pixel)
+    else :
+        print("Il n'y a pas d'absorption")
+
+
+# def eject_electron(ejection_test, grain_size):
+#     ejection = True
+#     nc = ((4 / 3) * np.pi * (grain_size ** 3) * c.RHO_C) / (c.M_C)
+#     gap = 4.4 + (c.Z_ELECTRON + 0.5) * (11.1) / (nc**(1 / 3))
+#     if ejection_test > nc:
+#         ejection = True
+#     else:
+#         ejection = False
+#     return ejection
--- Version Alpha/main.py
+++ Version Alpha/main.py
@@ -1,13 +1,44 @@
+################################################################################
+################################### IMPORT #####################################
+################################################################################
 import fonctions as f
+import constants as c
 from matplotlib import pyplot as plt
 import numpy as np
 from random import randrange
+from random import uniform
 
-matrix = f.load("Grain_N10_S2p0_B5p0.txt")
+################################################################################
+################################ INITIALIZATION ################################
+################################################################################
+energy = uniform(3, 16)
+da = f.non_uniform_generator_exp()
+grain_size = uniform(c.GRAIN_MIN_RADIUS, c.GRAIN_MAX_RADIUS)
+
+
+
+
+################################################################################
+matrix = f.load("Grain_N50_S0p1_B5p0.txt")
 nb_rows, nb_column = matrix.shape
 
-x_init = randrange(0, nb_rows)
 
-row = matrix[x_init]
+photon_init_position = randrange(0, nb_rows)
+row = matrix[photon_init_position]
 
-energy = randrange(3, 16)
+################################################################################
+################################ PRINT TEST ####################################
+################################################################################
+print("La taille du grain est de : ", grain_size, "m")
+print("le photon arrive sur la ligne n°", photon_init_position)
+
+
+contact_position, exit = f.x_contact(row)
+if not exit :
+    print("le photon rencontre le grain en position", contact_position)
+    print("le photon est absorbé au pixel ",f.x_absorption(da, matrix,
+          grain_size, contact_position))
+
+    # ejection_test = f.non_uniform_generator_th()
+    # ejection = f.eject_electron(ejection_test, grain_size)
+    # print(ejection)
